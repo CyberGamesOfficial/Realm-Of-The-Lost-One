a7cf13dcd84dc1f5fa9a901dafbb19d7623e11bb
Stella <jacob.bland@bostongrammarschool.co.uk>
2026-02-13 13:58:41 +0000

    Add players class with player initialization method

---
+++ libs/players.py
@@ -0,0 +1,19 @@
+class players:
+  def __init__(self, name):
+    self.name = name
+
+  def Player(name):
+    player = {
+      "name": name,
+      "power": 5,
+      "defence": 2,
+      "inventory": {
+        "backpack": [],
+        "head": None
+        "body": None
+        "legs": None
+      },
+      "alive": True
+    
+    }
+    return player
